37fbcae69a4cb73c3d106cfe8b1b5610a15cb936
Leticia Leal <leticia.leal@growdev.com.br>
2022-12-20 23:46:35 -0300

    adiciona possiveis resoluções para a atividade 4 - buscar valores repetidos

--- aula-20.12-Metodos-Arrays/index.js
+++ aula-20.12-Metodos-Arrays/index.js
@@ -15,11 +15,11 @@ let listaNumeros = [ -31, 18, 50, 98, 4, -53, 1, -8, 15, 89, 24, -3, 0, 10, 15, 
     console.log(impares)
     
 // 4 - Escreva um algoritmo para buscar apenas os números repetidos na lista
-// FILTER
-let repetidos = listaNumeros.filter((valor1, indice1, array1) => {
-    //
+
+// ---------------      PRIMEIRA RESOLUÇÃO  --------------------------
+let repetidos1 = listaNumeros.filter((valor1, indice1, array1) => {
     
-    console.log(`PRIMEIRA EXECUÇÃO - [${indice1}] = ${valor1}`)
+    // console.log(`FILTER - [${indice1}] = ${valor1}`)
    
 
     // SOME
@@ -28,11 +28,9 @@ let repetidos = listaNumeros.filter((valor1, indice1, array1) => {
          let indice = (indice2 + indice1) + 1
 
         if(array2.length > indice) {
-            console.log(`SOME - [${indice}] = ${array2[indice]}`) 
+            // console.log(`SOME - [${indice}] = ${array2[indice]}`) 
             return array2[indice] === valor1
         }
-
-        // console.log(`SOME - [${indice}] = ${array2[indice]}`) 
 
         return false
     })
@@ -40,7 +38,43 @@ let repetidos = listaNumeros.filter((valor1, indice1, array1) => {
     return repetiu
 })
 
-console.log(repetidos)
+console.log('')
+console.log('PRIMEIRA RESOLUÇÃO ', repetidos1)
+console.log('PRIMEIRA RESOLUÇÃO ', repetidos1.filter((valor, indice, array) => valor !== array[indice + 1]))
+
+
+// ---------------      SEGUNDA RESOLUÇÃO  --------------------------
+let repetidos2 = listaNumeros.filter((valorFilter, indiceFilter, arrayFilter) => {
+   
+    // SOME - true ou false
+    const repetiu = arrayFilter.some((valorSome, indiceSome) => {
+        
+        if(indiceSome === indiceFilter || indiceSome < indiceFilter) {
+            return false
+        }
+
+        return valorSome === valorFilter
+    })
+
+    return repetiu
+})
+
+console.log('')
+console.log('SEGUNDA RESOLUÇÃO ', repetidos2)
+console.log('SEGUNDA RESOLUÇÃO ', repetidos2.filter((valor, indice, array) => valor !== array[indice + 1]))
+
+
+// ---------------      TERCEIRA RESOLUÇÃO  --------------------------
+console.log('')
+
+let ordenados = listaNumeros.sort()
+console.log('TERCEIRA RESOLUÇÃO - ordena -> numeros iguais seguidos', ordenados)
+
+let repetidos3 = ordenados.filter((valor, indice, array) => valor === array[indice + 1])
+console.log('TERCEIRA RESOLUÇÃO ', repetidos3)
+console.log('TERCEIRA RESOLUÇÃO ', repetidos3.filter((valor, indice, array) => valor !== array[indice + 1]))
+
+
 
 // 5 - Escreva um algoritmo para informar se existe algum número maior que 50 na lista
 
@@ -51,6 +85,8 @@ const maiorQue50 = listaNumeros.some((value) => value > 50)
 // 6 - Escreva um algoritmo para informar se todos os números da lista são maiores que 50
     const igualOuMaiorQue = listaNumeros.every((value) => value > 50)
     console.log(igualOuMaiorQue)
+
+
 
 // 7 - Escreva um algoritmo para somar todos os números da lista e mostrar o resultado dessa soma
     let soma = listaNumeros.reduce((acc,valor)=>{
