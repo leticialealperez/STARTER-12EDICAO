a35f6e126cd1c7e8faec2f2c89d5068234993d70
Leticia Leal <leticia.leal@growdev.com.br>
2022-12-20 22:36:49 -0300

    Co-authored-by: Diego Lins <DiegoGLins@users.noreply.github.com> Co-authored-by: Stephan Costa <46Stephan@users.noreply.github.com> Co-authored-by: Marcos Túlio <ursopolar660@users.noreply.github.com> Co-authored-by: Roberto Carvalho de Souza Veloso <Roberto1501@users.noreply.github.com> Co-authored-by: Ramon Barth <RamonBarth1@users.noreply.github.com> Co-authored-by: RAFAEL PONSO <RafaelPonso@users.noreply.github.com> Co-authored-by: Vinicius Brodt <viniciusleandro@users.noreply.github.com> Co-authored-by: Diogo Dornas <diogodornas@users.noreply.github.com> Co-authored-by: Jonas Silva <jonas-amilton@users.noreply.github.com> Co-authored-by: Maycon Machado <mfideles@users.noreply.github.com> Co-authored-by: Thiago Donschat <Donschat@users.noreply.github.com>

--- aula-20.12-Metodos-Arrays/index.js
+++ aula-20.12-Metodos-Arrays/index.js
@@ -1,22 +1,75 @@
 let listaNumeros = [ -31, 18, 50, 98, 4, -53, 1, -8, 15, 89, 24, -3, 0, 10, 15, 47, 50, 15, 1, 4 ]
 
 // 1 - Escreva um algoritmo para buscar apenas os números positivos da lista
+    const positivos = listaNumeros.filter((valor, indice, array) => valor >= 0);
+    console.log(positivos);
 
+    
 // 2 - Escreva um algoritmo para buscar apenas os números pares e positivos da lista
+    let paresEPositivos = listaNumeros.filter((valor, indice, array) => valor%2 === 0 && valor > 0)
+    console.log(paresEPositivos) 
 
+    
 // 3 - Escreva um algoritmo para buscar apenas os números ímpares (positivos ou negativos) da lista
-
+    let impares = listaNumeros.filter((valor, indice, array) => valor % 2 !== 0)
+    console.log(impares)
+    
 // 4 - Escreva um algoritmo para buscar apenas os números repetidos na lista
+// FILTER
+let repetidos = listaNumeros.filter((valor1, indice1, array1) => {
+    //
+    
+    console.log(`PRIMEIRA EXECUÇÃO - [${indice1}] = ${valor1}`)
+   
+
+    // SOME
+    const repetiu = listaNumeros.some((valor2, indice2, array2) => {
+        
+         let indice = (indice2 + indice1) + 1
+
+        if(array2.length > indice) {
+            console.log(`SOME - [${indice}] = ${array2[indice]}`) 
+            return array2[indice] === valor1
+        }
+
+        // console.log(`SOME - [${indice}] = ${array2[indice]}`) 
+
+        return false
+    })
+
+    return repetiu
+})
+
+console.log(repetidos)
 
 // 5 - Escreva um algoritmo para informar se existe algum número maior que 50 na lista
 
+const maiorQue50 = listaNumeros.some((value) => value > 50)
+    console.log(maiorQue50);
+
+
 // 6 - Escreva um algoritmo para informar se todos os números da lista são maiores que 50
+    const igualOuMaiorQue = listaNumeros.every((value) => value > 50)
+    console.log(igualOuMaiorQue)
 
 // 7 - Escreva um algoritmo para somar todos os números da lista e mostrar o resultado dessa soma
+    let soma = listaNumeros.reduce((acc,valor)=>{
+    return acc+= valor
+    },0)
+
+    console.log(soma)
+
 
 // 8 - Escreva um algoritmo para buscar o índice do número 24
+const numero24 = listaNumeros.findIndex((valor) => valor == 24)
+console.log(numero24)
+
 
 // 9 - Escreva um algoritmo para buscar um número na lista que seja maior ou igual a 90
+const maiorIgual = listaNumeros.find((valor) => valor >= 90 ) 
+console.log(maiorIgual);
+
 
 // 10 - Escreva um algoritmo para percorrer a lista e criar uma nova lista, de mesmo tamanho, com os valores da lista ao quadrado
-
+let allList = listaNumeros.map ((valor, indice, array) => valor * valor)
+console.log(allList)
